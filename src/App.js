bdf99993a9bbef37407f0f844b39b24e02e5e5d5
jambis <imjaamees@gmail.com>
2019-09-11 20:15:11 -0500

    Met MVP with Datepicker

--- src/App.js
+++ src/App.js
@@ -1,13 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import APOD from "./components/APOD";
+import nasaAPOD from "./api/NasaAPOD";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState(null);
+  const [date, setDate] = useState("2019-09-11");
+
+  useEffect(() => {
+    nasaAPOD
+      .get("", {
+        params: { date: date }
+      })
+      .then(res => {
+        setData(res.data);
+      })
+      .catch(err => console.log(err));
+  }, [date]);
+
+  const handleDateChange = date => {
+    console.log(date);
+    function changeMonth(date) {
+      const months = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec"
+      ];
+      let monthNum = months.indexOf(date.substring(5, 8)) + 1;
+      return monthNum < 10 ? `0${monthNum}` : `${monthNum}`;
+    }
+
+    const regex = /(\w{3}) (\d{2}) (\d{4})/;
+    const regex2 = /[A-Za-z]+/;
+
+    let newDate = date
+      .toString()
+      .substring(4, 15)
+      .replace(regex, `$3-$1-$2`);
+
+    newDate = newDate.replace(regex2, changeMonth(newDate));
+
+    setDate(newDate);
+  };
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <h1>Nasa's Astronomy Picture of the Day</h1>
+      {data ? (
+        <APOD data={data} date={new Date(date)} callback={handleDateChange} />
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
---
+++ src/components/APOD.js
@@ -0,0 +1,59 @@
+import React from "react";
+import DatePicker from "react-date-picker";
+
+function APOD(props) {
+  const youtubeEmbed = (url, title) => {
+    return (
+      <iframe
+        title={title}
+        width="560"
+        height="315"
+        src={url}
+        frameBorder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    );
+  };
+  const renderContent = data => {
+    const { copyright, explanation, media_type, title, url } = data;
+    return (
+      <>
+        {media_type === "image" ? (
+          <img src={url} alt={title} />
+        ) : (
+          youtubeEmbed(url, title)
+        )}
+
+        <h2>{title}</h2>
+        <div className="desc">
+          <p className="desc">{explanation}</p>
+        </div>
+        <div className="footer">
+          <DatePicker
+            format="MM-dd-y"
+            value={new Date(props.date)}
+            clearIcon={null}
+            onChange={e => props.callback(e)}
+            maxDate={new Date()}
+            minDate={new Date("June 16, 1995")}
+          />
+          {copyright && media_type === "image" ? (
+            <p className="copyright">Image Credit & Copyright: {copyright}</p>
+          ) : null}
+          {copyright && media_type === "video" ? (
+            <p className="copyright">Video Credit & License: {copyright}</p>
+          ) : null}
+        </div>
+      </>
+    );
+  };
+  console.log(props);
+  return (
+    <div className="container">
+      {props.data ? renderContent(props.data) : <p>Loading...</p>}
+    </div>
+  );
+}
+
+export default APOD;
